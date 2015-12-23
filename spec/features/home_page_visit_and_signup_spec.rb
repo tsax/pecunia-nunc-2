ed5e007188f54fd0098f79753b5693becda40d6f
Tushar S <tsaxena86@gmail.com>
2015-12-23 04:05:35 -0500

    spec for mailer

--- spec/features/home_page_visit_and_signup_spec.rb
+++ spec/features/home_page_visit_and_signup_spec.rb
@@ -13,4 +13,22 @@ RSpec.feature "Home page visit", type: :feature do
     click_button "Subscribe!"
     expect(page).to have_text "Thanks for subscribing!"
   end
+
+  describe "When user subscribes" do
+    before(:all) do
+      visit root_path
+      fill_in "Name", with: "Dummy"
+      fill_in "Email", with: "dummy@dummy.com"
+      page.check "All Categories"
+      click_button "Subscribe!"
+    end
+
+    scenario "should have confirmation text" do
+      expect(page).to have_text "Thanks for subscribing!"
+    end
+
+    scenario "a confirmation email should be sent" do
+      expect(ActionMailer::Base.deliveries.count).to eq(1)
+    end
+  end
 end
