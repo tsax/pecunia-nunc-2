8c4e17a081965faed12cdd3a54353a3db3ce013d
Tushar S <tsaxena86@gmail.com>
2016-04-07 17:06:31 -0400

    make project_retriever methods into class methods

--- app/classes/project_retriever.rb
+++ app/classes/project_retriever.rb
@@ -1,7 +1,7 @@
 require 'kickstarter'
 
 class ProjectRetriever
-  def get_all_unfunded_projects_ending_soon
+  def self.get_all_unfunded_projects_ending_soon
     all_projects_ending_soon = Kickstarter.by_list_ending_soon(pages: :all)
     projects = all_projects_ending_soon.select do |pr|
       ending_soon(pr) && above_pledge_threshold(pr)
@@ -11,19 +11,19 @@ class ProjectRetriever
 
   private
 
-  def ending_soon project
+  def self.ending_soon project
     project.pledge_deadline.strftime("%F") < three_days_ahead.strftime("%F")
   end
 
-  def above_pledge_threshold project
+  def self.above_pledge_threshold project
     project.pledge_percent > pledge_threshold
   end
 
-  def three_days_ahead
+  def self.three_days_ahead
     Time.now + 3 * 24 * 60 * 60
   end
 
-  def pledge_threshold
+  def self.pledge_threshold
     79.9
   end
 end
