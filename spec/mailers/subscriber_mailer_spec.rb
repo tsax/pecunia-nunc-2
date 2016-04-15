e773870aff5b52a69411399e4f655f46a80d81ac
Tushar S <tsaxena86@gmail.com>
2016-04-14 21:49:09 -0400

    change user to subscriber in the let statements

--- spec/mailers/subscriber_mailer_spec.rb
+++ spec/mailers/subscriber_mailer_spec.rb
@@ -3,9 +3,9 @@ require "rails_helper"
 RSpec.describe SubscriberMailer, type: :mailer do
 
   describe 'send daily listing' do
-    let(:user) { mock_model Subscriber, name: 'Tushar', email: 'tsaxena86@gmail.com', allcategories: true }
-    let(:projects) { UserToProjectsMapper.new.get_category_matched_projects_for_user(user)}
-    let(:mail) { SubscriberMailer.daily_listing(user, projects) }
+    let(:subscriber) { mock_model Subscriber, name: 'Tushar', email: 'tsaxena86@gmail.com', allcategories: true }
+    let(:projects) { UserToProjectsMapper.new.get_category_matched_projects_for_user(subscriber)}
+    let(:mail) { SubscriberMailer.daily_listing(subscriber, projects) }
   
     it 'renders the subject' do
       expect(mail.subject).to eq('Whatevah!')
