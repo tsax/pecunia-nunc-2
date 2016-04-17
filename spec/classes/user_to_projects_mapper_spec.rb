8e1264ca19abe4dd836b2d233ec212b0ed7fa36e
Tushar S <tsaxena86@gmail.com>
2016-04-17 13:51:05 -0400

    add UserToProjectsMapper spec skeleton

--- spec/classes/user_to_projects_mapper_spec.rb
+++ spec/classes/user_to_projects_mapper_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe UserToProjectsMapper do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:subscriber) { mock_model Subscriber, name: 'Tushar', email: 'tsaxena86@gmail.com', allcategories: true }
+  let(:projects) { described_class.new.get_category_matched_projects_for_user(subscriber) }
+
+  # it "contains certain projects" do
+  #   projects.
+  # end
 end
